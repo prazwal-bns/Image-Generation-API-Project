b6ea92de7fad6e4a2e2e4fce42b3121cfbadd214
prazwal-bns <prajwalbns15@gmail.com>
2025-11-11 08:19:39 +0545

    Added laravel breeze api authentication

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Http\Controllers\V1\PromptGenerationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\V1\PromptGenerationController;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
 
 
 // Route::middleware('auth:sanctum')->group(function () {
